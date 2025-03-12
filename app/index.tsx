c62c811a52692c1751c963f5621f56d33505343c
Rafael Dantas <rafaeldantasboeira@gmail.com>
2025-03-12 20:11:50 -0300

    Criando alert celular

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,20 @@
 import { Button } from "@/components/Button";
-import { Text, View } from "react-native";
+import { Text, View, Alert, TouchableOpacity } from "react-native";
 
 export default function Index() {
+  const showAlert = () => {
+    Alert.alert("Oi", "Olá, tudo bem?");
+  };
+
   return (
-    <View className="bg-white w-full h-full flex justify-center items-center gap-8">
-      <Text className="text-2xl font-bold text-yellow-600">
-        Teste Cor
-      </Text>
-    <Button>
-      <Text className="text-white">Clique aqui</Text>
-    </Button>
+    <View className="bg-white w-full h-full flex flex-col justify-center items-center gap-8">
+      <Text className="text-2xl font-bold text-yellow-600">UE</Text>
+      <TouchableOpacity
+        onPress={showAlert}
+        className="flex gap-4 border border-black px-2 py-4 rounded-3xl hover:border-red-600"
+      >
+        <Text className="text-black">Clique aqui</Text>
+      </TouchableOpacity>
     </View>
   );
 }
